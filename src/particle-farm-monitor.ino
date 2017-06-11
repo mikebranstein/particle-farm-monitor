0bb7b9c1d93b643e751d4401397fd0be534bb4b0
Mike Branstein <mike.branstein@gmail.com>
2017-06-10 22:20:46 -0400

    add npm and flash D7 led

--- src/particle-farm-monitor.ino
+++ src/particle-farm-monitor.ino
@@ -5,14 +5,26 @@
  * Date:
  */
 
+int led = D7;
+
 // setup() runs once, when the device is first turned on.
 void setup() {
   // Put initialization like pinMode and begin functions here.
 
+  Cellular.off();
+
+  pinMode(led, OUTPUT);
 }
 
 // loop() runs over and over again, as quickly as it can execute.
 void loop() {
   // The core of your code will likely live here.
+  digitalWrite(led, HIGH);
+
+  delay(500);
+
+  digitalWrite(led, LOW);
+
+  delay(500);
 
 }
